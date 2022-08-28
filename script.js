368144b391d03c7853d562dffe88a6ae17a3a5c3
Mouss <104678778+MoussCarter@users.noreply.github.com>
2022-08-28 09:54:23 +0200

    update 2808:js hobbies part/navbar for ES/EN phone version

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ window.addEventListener("scroll", () => {
   lastScroll = window.scrollY;
 });
 
+// --------------------- Appear On Scroll AOS
+AOS.init();
+
 // --------------------- TYPED
 var typed = new Typed(".typed", {
   strings: [
@@ -22,13 +25,47 @@ var typed = new Typed(".typed", {
   smartBackspace: true,
   //     loop: true,
 });
-// --------------------- DATE OF THE DAY
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-// document.getElementById("current_date").innerHTML =
-//   "En date du : " + day + "/" + month + "/" + year;
 
-// --------------------- Appear On Scroll AOS
-AOS.init();
+// --------------------- BURGER MENU
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".burger-menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  burger.classList.toggle("burger-close");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  burger.classList.toggle("burger-close");
+});
+
+// NEW TAB
+function NewTab() {
+  window.open("./mail.html", "Contactez-moi", "width=700, height=660");
+}
+// MOUSEMOVE EVENT
+
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  innerCursor.style.top = e.pageY + "px";
+  innerCursor.style.left = e.pageX + "px";
+  outerCursor.style.top = e.pageY + "px";
+  outerCursor.style.left = e.pageX + "px";
+});
+
+// DISPLAY HOBBIE DESCRIPTION
+
+function displayDesc(categorie, bouton) {
+  document.getElementById(categorie, bouton);
+
+  if (categorie.style.display === "none") {
+    categorie.style.display = "block";
+    bouton.innerHTML = "Cacher";
+  } else {
+    categorie.style.display = "none";
+    bouton.innerHTML = "Lire plus...";
+  }
+}
--- script_es.js
+++ script_es.js
@@ -11,6 +11,9 @@ window.addEventListener("scroll", () => {
   lastScroll = window.scrollY;
 });
 
+// --------------------- Appear On Scroll AOS
+AOS.init();
+
 // --------------------- TYPED
 var typed = new Typed(".typed", {
   strings: [
@@ -22,5 +25,32 @@ var typed = new Typed(".typed", {
   smartBackspace: true,
 });
 
-// --------------------- Appear On Scroll AOS
-AOS.init();
+// --------------------- BURGER MENU
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".burger-menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  burger.classList.toggle("burger-close");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  burger.classList.toggle("burger-close");
+});
+
+// NEW TAB
+function NewTab() {
+  window.open("./mail.html", "Contactez-moi", "width=700, height=660");
+}
+// MOUSEMOVE EVENT
+
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  innerCursor.style.top = e.pageY + "px";
+  innerCursor.style.left = e.pageX + "px";
+  outerCursor.style.top = e.pageY + "px";
+  outerCursor.style.left = e.pageX + "px";
+});
